bfc3f3aa23b2ac36a8b432567eb441ac3014b473
amiramahmedjan <mahmedjan.amira@gmail.com>
2022-10-07 19:37:46 +0600

    Files uploading update

--- src/main/java/kz/ks/pricefeed/upload/UploadApplication.java
+++ src/main/java/kz/ks/pricefeed/upload/UploadApplication.java
@@ -1,13 +1,25 @@
 package kz.ks.pricefeed.upload;
 
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import kz.ks.pricefeed.upload.service.FilesStorageService;
+
 @SpringBootApplication
-public class UploadApplication {
+public class UploadApplication implements CommandLineRunner {
+	@Resource
+	FilesStorageService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UploadApplication.class, args);
 	}
 
+	@Override
+	public void run(String... arg) throws Exception {
+		storageService.deleteAll();
+		storageService.init();
+	}
 }
